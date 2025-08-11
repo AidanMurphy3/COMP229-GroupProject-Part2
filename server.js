4e9b50bdcb5b1d15fa49a286f2508feecb52852e
RandomSpagety <nguyenphanmk73@gmail.com>
2025-08-10 23:52:43 -0400

    switch port, auth now works
    now using port 8080, if there is still getting 403 forbiden, continue to switch port by changing .env file and line 32 of server.js

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server started on port ${PORT}`));
